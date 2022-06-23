aeb149c59875434e46c80c44445c41e16ea4eab3
DaliusDeveikis <slkyper@gmail.com>
2022-06-23 17:39:33 +0300

    Reaktyviųjų formų modulis

---
+++ src/app/components/new-recipe/new-recipe.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-recipe',
+  templateUrl: './new-recipe.component.html',
+  styleUrls: ['./new-recipe.component.css'],
+})
+export class NewRecipeComponent implements OnInit {
+  public recipeForm: FormGroup;
+
+  constructor() {
+    this.recipeForm = new FormGroup({
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(30),
+      ]),
+      preparationTime: new FormControl(null, [
+        Validators.required,
+        this.timeValidator,
+      ]),
+      description: new FormControl(null, Validators.required),
+      image: new FormControl(null, [Validators.required, this.urlValidator]),
+      kcal: new FormControl(null),
+    });
+  }
+
+  ngOnInit(): void {}
+
+  timeValidator(control: FormControl): { [s: string]: boolean } | null {
+    if (control.value % 5) {
+      return { mustBe5MinIntervals: true };
+    } else {
+      return null;
+    }
+  }
+
+  urlValidator(control: FormControl): { [s: string]: boolean } | null {
+    const regex = new RegExp(
+      /(https:\/\/)([^\s(["<,>/]*)(\/)[^\s[",><]*(.png|.jpg)(\?[^\s[",><]*)?/g
+    );
+    if (!regex.test(control.value)) {
+      return { mustBeValidURL: true };
+    } else {
+      return null;
+    }
+  }
+
+  public addRecipe() {
+    console.log(this.recipeForm);
+  }
+}
